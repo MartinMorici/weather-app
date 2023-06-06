607c33af9d33d5662da51ac130fdb1cbd9aaa98c
Martín Morici <martinmorici3@gmail.com>
2023-06-06 11:46:40 -0300

    estilos terminados

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { Location, Weather } from './types';
-import { cities } from './cities';
+import { Location, Weather } from './utilities/types';
+import { cities } from './utilities/cities';
 import Water from './assets/water-solid.svg';
 import Wind from './assets/wind-solid.svg';
 import Cloudsun from './assets/cloud-sun-rain-solid.svg';
@@ -47,8 +47,8 @@ function App() {
       const resp = await data.json();
       console.log(resp);
 
-      const { 0: first, 8: second, 16: third, 24: fourth, 32: fifth } = resp.list;
-      const filteredList = [first, second, third, fourth, fifth];
+      const { 0: first, 8: second, 16: third, 24: fourth, 32: fifth, 39: sixth } = resp.list;
+      const filteredList = [first, second, third, fourth, fifth, sixth];
 
       setWeather({ ...resp, list: filteredList });
       setActive(filteredList[0].dt);
@@ -61,31 +61,33 @@ function App() {
   }, [location]);
 
   if (loading) {
-    return <div className='spinner'></div>;
+    return <div className='spinner mx-auto w-full'></div>;
   }
 
   return (
     <main className='w-full'>
-      {!weather && <button onClick={getCurrentLocation}>Get Weather In Your Location</button>}
+      {!weather && (
+        <>
+          <button className='text-xl' onClick={getCurrentLocation}>Get Weather In Your Location</button>
+          <h2 className='my-6 font-bold text-2xl'>OR</h2>
+        </>
+      )}
+      <select className='px-10 py-[0.6em] text-xl' onChange={handleSelectedCity}>
+        <option> Seleccione una ciudad</option>
+        {cities.map((city) => (
+          <option key={city.id} value={city.id}>
+            {city.name}
+          </option>
+        ))}
+      </select>
       {weather && (
-        <section>
-          <select onChange={handleSelectedCity}>
-            <option> Seleccione una ciudad</option>
-            {cities.map((city) => (
-              <option key={city.id} value={city.id}>
-                {city.name}
-              </option>
-            ))}
-          </select>
 
-          <h1>{weather.city.name}</h1>
-
-          <section className='flex justify-center'>
+          <section className='flex justify-center h-[300px] mt-8'>
             {weather.list.map((day) => {
               const fecha = new Date(day.dt * 1000);
               return active === day.dt ? (
-                <article className='rounded-l-xl bg-[#323546] mb-4 max-w-[400px] w-full' key={day.dt}>
-                  <header className='flex justify-between rounded-tl-xl py-2 px-4 bg-[#2C2F3E]'>
+                <article className='cursor-pointer even:bg-[#272936] bg-[#323546] mb-4 max-w-[400px] w-full transition-all' key={day.dt}>
+                  <header className='even:bg-[#272936] flex justify-between py-2 px-4 bg-[#2C2F3E]'>
                     <span>{fecha.toLocaleString('en-EN', { weekday: 'long' })}</span>
                     <div className='flex gap-1'>
                       <span>{fecha.toLocaleString('en-EN', { day: 'numeric' })}</span>
@@ -95,8 +97,8 @@ function App() {
                   <main className=' px-4 mb-7 '>
                     <h2 className='text-left pt-6 mb-4 '>{weather.city.name}</h2>
                     <div className='flex items-end'>
-                      <h2 className='font-bold text-6xl mt-[-5px]'>{day.main.temp.toFixed(0)}°C</h2>
-                      <img className='w-28 h-[3.5rem] object-cover mb-[-2px] ml-4' src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt='' />
+                      <h2 className='font-bold text-8xl mt-[-5px]'>{day.main.temp.toFixed(0)}°C</h2>
+                      <img className='w-32 h-[90px] object-cover  ml-4' src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt='' />
                     </div>
                   </main>
                   <footer className='flex text-sm items-center gap-4 px-4 pb-8 text-[#7A7D84] font-semibold '>
@@ -115,21 +117,21 @@ function App() {
                   </footer>
                 </article>
               ) : (
-                <article className='flex flex-col justify-center items-center bg-[#323546] mb-4 max-w-[150px] w-full' key={day.dt} onClick={() => setActive(day.dt)}>
-                  <header className='flex justify-center py-2 px-4 bg-[#2C2F3E] w-full'>
+                <article className='cursor-pointer  even:bg-[#272936] flex flex-col justify-center items-center bg-[#323546] mb-4 max-w-[150px] w-full transition-all' key={day.dt} onClick={() => setActive(day.dt)}>
+                  <header className='even:bg-[#272936] flex justify-center py-2 px-4 bg-[#2C2F3E] w-full'>
                     <span>{fecha.toLocaleString('en-EN', { weekday: 'long' })}</span>
                   </header>
                   <main className='flex-1 px-4 mb-7 justify-center items-center flex '>
                     <div className='flex flex-col  justify-center items-center'>
                       <img className='w-22 h-[3.5rem] object-cover mb-2 ml-4' src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt='' />
                       <h2 className='font-bold text-4xl mt-[-5px]'>{day.main.temp.toFixed(0)}°C</h2>
+                      <h3 className='text-[#7A7D84] font-semibold mt-1 text-lg'>{Math.round(day.main.temp_min)} °C</h3>
                     </div>
                   </main>
                 </article>
               );
             })}
           </section>
-        </section>
       )}
     </main>
   );
